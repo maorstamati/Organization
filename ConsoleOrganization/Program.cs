24b703f8d1aaa87438a9091380bfeffb5a1f2aa8
Maor Stamati <maors@il.ibm.com>
2017-11-14 18:35:37 +0200

    Add project files.

---
+++ ConsoleOrganization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organization;
+
+namespace ConsoleOrganization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = Boolean.Parse(args[0]);
+
+            Manager objManager = new Manager();
+
+            SwipeCard objSwipeCard = new SwipeCard();
+
+            Project obj = new Project(objManager);
+
+            System.Console.WriteLine("Initial Salary of Manager is: {0}", objManager.Salary);
+            objManager.HowisTheManager(success);
+
+            if (success)
+            {
+                System.Console.WriteLine("Manager Salary after project Success is: {0}", objManager.Salary);
+            }
+            else
+            {
+                System.Console.WriteLine("Manager Salary after project Failure is: {0}", objManager.Salary);
+            }
+            System.Console.WriteLine();
+
+        }
+    }
+}
---
+++ Organization/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace Organization
+{
+    public class Manager
+    {
+        public Project oProject;
+
+        public Manager()
+        {
+            oProject = new Project(this);
+        }
+
+        //Aggregation relationship
+        public List<Worker> Workers = new List<Worker>();
+        public void Logon(SwipeCard obj)
+        {
+            obj.Swipe(this);
+        }
+
+        public string GetManagerName()
+        {
+            return "Shiv";
+        }
+
+        public double Salary = 10;
+
+        public void HowisTheManager(bool Good)
+        {
+            if (Good)
+            {
+                oProject.Success = true;
+            }
+            else
+            {
+                oProject.Success = false;
+            }
+        }
+
+    }
+
+    public class SwipeCard
+    {
+        public void Swipe(Manager obj)
+        {
+            // goes and swipes the manager
+        }
+
+        public string MakeofSwipecard()
+        {
+            return "c001";
+        }
+
+    }
+
+    public class Worker
+    {
+        public string Wrokername = "";
+    }
+
+    public class Project
+    {
+
+        private Manager oManager;
+        private bool IsSuccess = false;
+
+        public bool Success
+        {
+            get { return IsSuccess; }
+            set
+            {
+                IsSuccess = value;
+                if (value)
+                {
+                    oManager.Salary++;
+                }
+                else
+                {
+                    oManager.Salary--;
+                }
+            }
+        }
+
+        public Project(Manager obj)
+        {
+            oManager = obj;
+        }
+    }
+}
+    
+    
+
+
+
+
